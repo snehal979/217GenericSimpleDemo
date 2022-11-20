3c7d9b39d4e411d5eb4183ba8b9a0d6150867089
snehal979 <113917713+snehal979@users.noreply.github.com>
2022-11-20 08:47:56 +0530

    Demo for Generic

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 5, 4, 6, 7 };
+            double[] doubleArray = { 1.1, 2.2, 2.4, 3.6, 3.5 };
+            char[] charArray = { 'a', 'b', 'e', 'r' };
+
+            Generic generic = new Generic();
+            generic.ToprintArray<int>(intArray);
+            generic.ToprintArray<double>(doubleArray);
+            generic.ToprintArray<char>(charArray);
+        }
+    }
+}
